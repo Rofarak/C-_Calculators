86b15add44bcb8c6ee01737a71610446b8a30e88
Rofarak <Atirox@ya.ru>
2025-10-23 09:23:34 +0500

    Class_work

---
+++ CalculatorBasic/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalculatorBasic.Interface;
+using CalculatorBasic.Classes;
+while (true) {
+    ICalculatorBasic _calc = new calculator();
+    Console.WriteLine("did you want App?");
+    var answer = Console.ReadLine().Trim().ToLower(); 
+    if  (answer == "yes")
+    {
+        //write programm
+        Console.WriteLine(_calc.Addition(5, 5));
+    }
+    else
+    {
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+}
